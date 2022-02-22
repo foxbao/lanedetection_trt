95b7a42f3d45bd24baf30e93e607336f5fcc82cb
foxbao <jidushanbojue1776@hotmail.com>
2022-02-22 22:20:04 +0800

    morphological

---
+++ src/lanecluster.cpp
@@ -0,0 +1,22 @@
+#include "lanecluster.h"
+
+
+LaneCluster::LaneCluster()
+{
+    
+}
+
+void LaneCluster::apply_lane_feats_cluster()
+{
+
+}
+
+void LaneCluster::embedding_feats_dbscan_cluster()
+{
+
+}
+
+void LaneCluster::get_lane_embedding_feats()
+{
+
+}
---
+++ src/lanecluster.h
@@ -0,0 +1,11 @@
+
+class LaneCluster
+{
+    public:
+        LaneCluster();
+        void embedding_feats_dbscan_cluster();
+        void get_lane_embedding_feats();
+        void apply_lane_feats_cluster();
+    private:
+
+};
