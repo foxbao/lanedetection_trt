bb10583e8d42b668d437cebd00816c2b79a692ca
foxbao <jidushanbojue1776@hotmail.com>
2022-03-02 01:15:43 +0800

    release mode

---
+++ build/bin/CMakeFiles/dbscan.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dbscan.dir/dbscan.cpp.o"
+  "libdbscan.pdb"
+  "libdbscan.so.1.2"
+  "libdbscan.so"
+  "libdbscan.so.1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dbscan.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
